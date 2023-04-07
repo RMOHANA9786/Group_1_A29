0bb84573ec3060dacf9a78d1e014977afa7efa4c
RMOHANA9786 <127104852+RMOHANA9786@users.noreply.github.com>
2023-04-07 05:58:04 -0700

    Add files via upload

---
+++ task3mohana/SocialMediamohana.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './SocialMedia.css'
+
+function SocialMedia() {
+    return (<>
+        <div className="footer-icon-sidebar">
+            <ul className="navbar-nav">
+                <li className="nav-item inline-block">
+                    <a href="https://github.com/RMOHANA9786/Group_1_A29.git" aria-label="Github" className="nav-link text-github"><i
+                        className="fa fa-github"></i></a></li>
+                <li className="nav-item inline-block">
+                    <a href="https://medium.com/@mohana9786142078" aria-label="Medium" className="nav-link">
+
+                        <span><img className="img-circle medium-icon" width="24" alt=""
+                            src="https://seeklogo.com/images/M/medium-logo-93CDCF6451-seeklogo.com.png" /></span></a>
+                </li>
+                <li className="nav-item inline-block"><a href="https://instagram.com/monisha_o_a_a?igshid=ZDdkNTZiZTM=/" aria-label="Insagram" className="nav-link"><i className="fa fa-instagram"
+                ></i></a>
+                </li>
+                <li className="nav-item inline-block"><a href="https://www.linkedin.com/in/r-mohana-ab02b825a/" aria-label="LinkedIn" className="nav-link"><i className="fa fa-linkedin"
+                ></i></a>
+                </li>
+            </ul>
+        </div>
+
+
+    </>
+    )
+}
+
+export default SocialMedia
