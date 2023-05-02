4d706834e111c3ab205ae88e5c10faa6e0fac833
RMOHANA9786 <127104852+RMOHANA9786@users.noreply.github.com>
2023-05-02 04:39:08 -0700

    Update footer.js

--- src/Containers/footer/footer.js
+++ src/Containers/footer/footer.js
@@ -7,7 +7,7 @@ function footer() {
     return (
         <>
             <footer className="fadeInUp col-md-12">
-                <a href="https://github.com/sowmiya-saravanan1/Group_A29_2" className="button github"><svg xmlns="http://www.w3.org/2000/svg"
+                <a href="https://github.com/RMOHANA9786/Group_1_A29.git" className="button github"><svg xmlns="http://www.w3.org/2000/svg"
                     width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
                     strokeLinecap="round" strokeLinejoin="round">
                     <path
